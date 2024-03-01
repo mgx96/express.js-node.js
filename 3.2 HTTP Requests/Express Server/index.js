78f983dbab6f0689c5b3f30ba378ee29ed25a86c
Malek Sharabi <m.malek1996@hotmail.com>
2024-03-01 23:18:01 +0100

    Update index.js

--- 3.2 HTTP Requests/Express Server/index.js
+++ 3.2 HTTP Requests/Express Server/index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.send("This is a server running on Mike's computer");
 });
 
+app.get("/about", (req, res) => {
+    res.send("I am currently the head of developers at Lucidia and I am a game developer and a full stack developer. I also have some experience in smart contracts and solidity");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("You can contact me at malek.sharabi@lucidia.io")
+})
+
 app.listen(3000, () =>{
     console.log(`Server running on port ${port}.`);
 });
