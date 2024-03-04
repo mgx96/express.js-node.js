d38f279b89ddb8f7ae0adfc902328c2bd545e25e
Malek Sharabi <m.malek1996@hotmail.com>
2024-03-04 23:54:41 +0100

    Update index2

--- 3.4+Middleware/3.4 Middleware/index2.js
+++ 3.4+Middleware/3.4 Middleware/index2.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  console.log("Request method: ", req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
