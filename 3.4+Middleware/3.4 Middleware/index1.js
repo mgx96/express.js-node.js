a516cda84c0bf7cdfa21cfb0ac485c61a7db1e00
Malek Sharabi <m.malek1996@hotmail.com>
2024-03-04 20:47:27 +0100

    Update index3

--- 3.4+Middleware/3.4 Middleware/index1.js
+++ 3.4+Middleware/3.4 Middleware/index1.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
+import morgan from "morgan";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+app.use(morgan("tiny"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
